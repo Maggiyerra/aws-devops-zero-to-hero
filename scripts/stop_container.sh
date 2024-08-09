a87f6f3c8b1e943cf5fd06b3437470eebf047e7b
Maggiyerra <152356122+Maggiyerra@users.noreply.github.com>
2024-08-09 10:31:53 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-commitid = docker ps | awk -F" " '[print $1]'
-docker rm -f $commitid
+containerid='docker ps | awk -F " " '{print $1}''
+docker rm -f $containerid
